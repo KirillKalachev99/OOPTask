9b4975b5cfaf56fca40e7deb59e94cbb5c0e8b81
Kirik99 <kirik-kirik99@mail.ru>
2025-05-05 23:53:01 +0300

    ADD & UPDATE added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 package ru.netology
 
 fun main() {
+    val wall1 = WallService
     val comment1 = Comment(10)
     val likes1 = Like(200)
 
     val post1 = Post(1, 2, 3, 5052025, "Текст первого поста!", 4, 5, true, comment1, likes1)
+    val post2 = Post(2, 2, 3, 5052025, "Обновленный текст первого поста!", 4, 5, true, comment1, likes1)
+    wall1.add(post1)
+    wall1.update(post2)
 
-    println(post1.likes)
-    
+    println(wall1.toString())
 }
--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -2,4 +2,27 @@ package ru.netology
 
 object WallService {
     private var posts = emptyArray<Post>()
+
+    fun add(post: Post): Post {
+        posts += post
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((i, p) in posts.withIndex()) {
+            if (post.id == p.id) {
+                posts[i] = post
+                return true
+            } else {
+                println("Вы пытаетесь обновить несуществующий пост!")
+                return false
+            }
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return posts.contentDeepToString()
+    }
 }
+
