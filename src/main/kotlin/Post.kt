dde018b07ae3fbe744333689260052b27f88350a
Kirik99 <kirik-kirik99@mail.ru>
2025-05-13 00:22:51 +0300

    Nullable deleted

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -5,10 +5,10 @@ data class Post(
     val fromId: Int,
     val date: Int,
     val text: String,
-    val replyOwnerId: Int? = null,
-    val replyPostId: Int? = null,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
     val friendsOnly: Boolean = true,
-    val comments: Comment? = null,
+    val comments: Comment,
     val likes: Like,
     var id: Int = 0
 )
